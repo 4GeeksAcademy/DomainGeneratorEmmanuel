28f6dfc05ceeb3e5fbea960b707700cd2d2aee0e
Emmanuel Vargas <126733106+EmmanuelV22@users.noreply.github.com>
2023-05-16 15:52:57 +0000

    New changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+function randomDomain(array) {
+  let randomPosition;
+  randomPosition = Math.floor(Math.random() * array.length);
+  return randomPosition;
+}
+
+function total() {
+  let domainTotal = [];
+  domainTotal =
+    pronoun[randomDomain(pronoun)] +
+    adj[randomDomain(adj)] +
+    noun[randomDomain(noun)] +
+    ".com";
+  return domainTotal;
+}
+let elementModify = document.querySelector("#changeText");
+
+elementModify.addEventListener("click", () => {
+  elementModify.innerHTML = total();
+});
